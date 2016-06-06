53a8c1d19a6cabcc369bc09e775fc61f86d338e4
abiaravi <abinayashanmugam17@gmail.com>
2016-06-06 11:19:45 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	if(n<0)
+	{
+		printf("if the number is positive");
+	}
+	else if(n==0)
+	{
+		printf("if the number is zero");
+	}
+	else
+	{
+		printf("if the number is negatie");
+	}
+}
