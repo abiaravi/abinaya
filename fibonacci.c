ccd61662ade4724830596cefafb2a9c2ad519d4a
abiaravi17 <abinayashanmugam17@gmail.com>
2016-06-07 15:50:48 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i, n, t1 = 0, t2 = 1, nextTerm = 0;
+    scanf("%d",&n);
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    for (i=3; i <= n; ++i)
+    {
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        printf("%d, ",nextTerm);
+    }
+    return 0;
+}
